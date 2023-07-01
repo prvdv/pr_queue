45cf05a945136691115ce31bcaede9a223b1cedc
Andrey Parvadov <parvadov2004@gmail.com>
2023-07-01 21:01:30 +0300

    Реализована функция heapifyDown

--- pr_queue.cpp
+++ pr_queue.cpp
@@ -10,3 +10,18 @@ void pr_queue::heapifyUp(int index){
 		heapifyUp(parent);
 	}
 }
+void pr_queue::heapifyDown(int index){
+    int left = 2 * index + 1;
+	int right = 2 * index + 2;
+	int min = index;
+	if (left < heap.size() && heap[left] > heap[min]) {
+		min = left;
+	}
+	if (right < heap.size() && heap[right] > heap[min]) {
+		min = right;
+	}
+	if (min != index) {
+		std::swap(heap[index], heap[min]);
+		heapifyDown(min);
+	}
+}
